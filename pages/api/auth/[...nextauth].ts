48e3f1295ff21501b07ccf8d1006291306db9fab
eunnho <taemin4u@khu.ac.kr>
2023-06-06 13:02:28 +0900

    fix: env file

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,8 +6,8 @@ const nextAuthOptions = (): NextAuthOptions => {
   return {
     providers: [
       GoogleProvider({
-        clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '',
-        clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET || '',
+        clientId: process.env.GOOGLE_CLIENT_ID || '',
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
       }),
     ],
     secret: 'HelloWorld',
