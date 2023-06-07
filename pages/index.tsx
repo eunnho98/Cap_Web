b433ff9a083ab9022a05db1232601c6260ae918b
eunnho <taemin4u@khu.ac.kr>
2023-06-08 02:52:33 +0900

    fix: login error

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,44 +14,44 @@ function home() {
 
   if (status === 'authenticated') {
     router.push('/login');
-  }
-
-  return (
-    <VStack p="200px 40px" gap={4}>
-      <SlideFade
-        in={true}
-        offsetY="20px"
-        transition={{ enter: { duration: 0.5 } }}
-        onAnimationComplete={() => {
-          setShade(true);
-        }}
-      >
-        <Heading textAlign="center" fontSize="42px">
-          YoKHURoute
-        </Heading>
-      </SlideFade>
-      <SlideFade
-        in={shade}
-        offsetY="20px"
-        transition={{ enter: { duration: 0.5 } }}
-      >
-        <Button
-          w="240px"
-          h="50px"
-          fontSize="24px"
-          letterSpacing="2px"
-          lineHeight="32px"
-          display="block"
-          colorScheme="purple"
-          onClick={() => {
-            router.push('/auth');
+  } else {
+    return (
+      <VStack p="200px 40px" gap={4}>
+        <SlideFade
+          in={true}
+          offsetY="20px"
+          transition={{ enter: { duration: 0.5 } }}
+          onAnimationComplete={() => {
+            setShade(true);
           }}
         >
-          시작하기
-        </Button>
-      </SlideFade>
-    </VStack>
-  );
+          <Heading textAlign="center" fontSize="42px">
+            YoKHURoute
+          </Heading>
+        </SlideFade>
+        <SlideFade
+          in={shade}
+          offsetY="20px"
+          transition={{ enter: { duration: 0.5 } }}
+        >
+          <Button
+            w="240px"
+            h="50px"
+            fontSize="24px"
+            letterSpacing="2px"
+            lineHeight="32px"
+            display="block"
+            colorScheme="purple"
+            onClick={() => {
+              router.push('/auth');
+            }}
+          >
+            시작하기
+          </Button>
+        </SlideFade>
+      </VStack>
+    );
+  }
 }
 
 export default home;
